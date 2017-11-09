18a1948acc9e015a5a58625661fe9b20fb0aebda
Jack Footner <Jackrfootner@gmail.com>
2017-11-10 10:29:43 +1030

    Basic Layout Switch to C#

---
+++ DiscordRPGui/frmLogin.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DiscordRPGui
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.cmdLogin = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.picDRPGLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDRPGLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(147, 326);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // cmdLogin
+            // 
+            this.cmdLogin.Location = new System.Drawing.Point(12, 326);
+            this.cmdLogin.Name = "cmdLogin";
+            this.cmdLogin.Size = new System.Drawing.Size(75, 23);
+            this.cmdLogin.TabIndex = 12;
+            this.cmdLogin.Text = "Login";
+            this.cmdLogin.UseVisualStyleBackColor = true;
+            this.cmdLogin.Click += new System.EventHandler(this.cmdLogin_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(13, 281);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 11;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(13, 229);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 10;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 300);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(210, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 245);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(210, 20);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // picDRPGLogo
+            // 
+            this.picDRPGLogo.Location = new System.Drawing.Point(12, 12);
+            this.picDRPGLogo.Name = "picDRPGLogo";
+            this.picDRPGLogo.Size = new System.Drawing.Size(210, 210);
+            this.picDRPGLogo.TabIndex = 7;
+            this.picDRPGLogo.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 361);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.cmdLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.picDRPGLogo);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.picDRPGLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnClose;
+        internal System.Windows.Forms.Button cmdLogin;
+        internal System.Windows.Forms.Label lblPassword;
+        internal System.Windows.Forms.Label lblUsername;
+        internal System.Windows.Forms.TextBox txtPassword;
+        internal System.Windows.Forms.TextBox txtUsername;
+        internal System.Windows.Forms.PictureBox picDRPGLogo;
+    }
+}
---
+++ DiscordRPGui/frmMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordRPGui
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmdMine_Click(object sender, EventArgs e)
+        {
+            cmdMine.Enabled = false;
+            pgsMine.Value = 0;
+        }
+
+        private void cmdForage_Click(object sender, EventArgs e)
+        {
+            cmdForage.Enabled = false;
+            pgsForage.Value = 0;
+        }
+
+        private void cmdChop_Click(object sender, EventArgs e)
+        {
+            cmdChop.Enabled = false;
+            pgsChop.Value = 0;
+        }
+
+        private void cmdFish_Click(object sender, EventArgs e)
+        {
+            cmdFish.Enabled = false;
+            pgsFish.Value = 0;
+        }
+
+        private void tmrProgress_Tick(object sender, EventArgs e)
+        {
+            if(pgsMine.Value < pgsMine.Maximum)
+            {
+                pgsMine.Value++;
+                if(pgsMine.Value == pgsMine.Maximum)
+                {
+                    cmdMine.Enabled = true;
+                }
+            }
+            if (pgsForage.Value < pgsForage.Maximum)
+            {
+                pgsForage.Value++;
+                if (pgsForage.Value == pgsForage.Maximum)
+                {
+                    cmdForage.Enabled = true;
+                }
+            }
+            if (pgsChop.Value < pgsChop.Maximum)
+            {
+                pgsChop.Value++;
+                if (pgsChop.Value == pgsChop.Maximum)
+                {
+                    cmdChop.Enabled = true;
+                }
+            }
+            if (pgsFish.Value < pgsFish.Maximum)
+            {
+                pgsFish.Value++;
+                if (pgsFish.Value == pgsFish.Maximum)
+                {
+                    cmdFish.Enabled = true;
+                }
+            }
+        }
+
+        private void cmdAllActions_Click(object sender, EventArgs e)
+        {
+            if(chkMine.Checked && cmdMine.Enabled)
+            {
+                cmdMine.Enabled = false;
+                pgsMine.Value = 0;
+            }
+            if (chkForage.Checked && cmdForage.Enabled)
+            {
+                cmdForage.Enabled = false;
+                pgsForage.Value = 0;
+            }
+            if (chkChop.Checked && cmdChop.Enabled)
+            {
+                cmdChop.Enabled = false;
+                pgsChop.Value = 0;
+            }
+            if (chkFish.Checked && cmdFish.Enabled)
+            {
+                cmdFish.Enabled = false;
+                pgsFish.Value = 0;
+            }
+        }
+    }
+}
